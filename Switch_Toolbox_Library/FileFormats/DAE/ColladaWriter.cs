6861def05c4ca76537dac0535aa4a1e11263b4fc
ranidspace <ranidspace@proton.me>
2024-02-04 15:17:56 -0500

    testing exporting dae files to 0.5 specular instead of 0

--- Switch_Toolbox_Library/FileFormats/DAE/ColladaWriter.cs
+++ Switch_Toolbox_Library/FileFormats/DAE/ColladaWriter.cs
@@ -228,7 +228,7 @@ namespace Toolbox.Library.Collada
                     if (!HasEmission)
                         WriteMaterialColor("emission", new float[] { 0, 0, 0, 1 });
                     if (!HasSpecular)
-                        WriteMaterialColor("specular", new float[] { 0, 0, 0, 1 });
+                        WriteMaterialColor("specular", new float[] { 0.5, 0.5, 0.5, 1 });
 
                     Writer.WriteEndElement();
                     Writer.WriteEndElement();
